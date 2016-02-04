3149fa908e6dd6b300ff1b320737be412e0f79dd
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-02-04 09:31:31 -0600

    Block 'only me' logging if current user not in game

--- client/views/options.js
+++ client/views/options.js
@@ -1,14 +1,31 @@
+Template.options.onRendered(function(){
+  var p1 = Session.get('player1');
+  var p2 = Session.get('player2');
+  var p3 = Session.get('player3');
+  var p4 = Session.get('player4');
+  var rstr = [p1, p2, p3, p4];
+  if (!_.contains(rstr, Meteor.userId())) {
+    console.log('you"re not in the game');
+    $('#soloPlayers').attr('disabled', 'disabled');
+    $('#soloPlayers').removeAttr('checked');
+    if ($('#allPlayers').attr('disabled') === 'disabled') {
+      $('.btn-next').addClass('disabled');
+    }
+  }
+});
+
 Template.options.events({
-  'click .btn-next': function () {
+  'click .btn-next:not(.disabled)': function () {
+    var rt = $('.record-type-form input[type=radio]:checked').attr('recordType');
+    var rp = $('.record-players-form input[type=radio]:checked').attr('recordPlayers');
     var p1 = Session.get('player1');
     var p2 = Session.get('player2');
     var p3 = Session.get('player3');
     var p4 = Session.get('player4');
-    var rt = $('.record-type-form input[type=radio]:checked').attr('recordType');
-    var rp = $('.record-players-form input[type=radio]:checked').attr('recordPlayers');
+    var rstr = [p1, p2, p3, p4];
     Games.insert({
       date: new Date(),
-      roster: [p1, p2, p3, p4],
+      roster: rstr,
       records: {
         p1: {hits: null, misses: null, glass: null},
         p2: {hits: null, misses: null, glass: null},

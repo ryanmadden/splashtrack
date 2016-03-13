1e44203460cc95a736296c1f9d0893e7199362c4
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-03-13 00:45:39 -0600

    Load fb profile pics over https

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,7 +4,7 @@ Meteor.startup(function () {
 
 Accounts.onCreateUser(function(options, user) {
     if (options.profile) {
-        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+        options.profile.picture = "https://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
         user.profile = options.profile;
     }
     return user;

48e37596debc159aef88e1282f9529cde19866fd
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-03-30 09:26:10 -0500

    Display toast on login error

--- client/views/core/login.js
+++ client/views/core/login.js
@@ -3,6 +3,7 @@ Template.login.events({
     Meteor.loginWithFacebook({ requestPermissions: ['email']},
       function (error) {
         if (error) {
+          Materialize.toast('Login error', 4000);
           return console.log(error);
         }
       });

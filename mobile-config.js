c691e568fed5cb171d57f023e0145b1aede9e262
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-03-31 15:38:52 -0500

    Add app resources

---
+++ mobile-config.js
@@ -0,0 +1,20 @@
+App.info({
+  name: 'SplashTrack',
+  description: 'Track your caps games and watch your stats improve!',
+  version: '1.0.0'
+});
+
+App.icons({
+  'iphone_2x': 'resources/icons/iphone_2x.png',
+  'iphone_3x': 'resources/icons/iphone_3x.png',
+  'ios_settings': 'resources/icons/ios_settings.png',
+  'ios_settings_2x': 'resources/icons/ios_settings_2x.png',
+  'ios_settings_3x': 'resources/icons/ios_settings_3x.png',
+});
+
+App.launchScreens({
+  'iphone_2x': 'resources/splash/640x960.png',
+  'iphone5': 'resources/splash/640x1136.png',
+  'iphone6': 'resources/splash/750x1334.png',
+  'iphone6p_portrait': 'resources/splash/1242x2208.png',
+});

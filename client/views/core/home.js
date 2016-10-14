d0e5c034e19d84fdd33ecb83886066b11fe5c2a6
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-10-14 13:53:11 -0500

    Revert "potw"
    This reverts commit d28a1df5aa6af5bc7449eeabd69d959ba483e2ff.

--- client/views/core/home.js
+++ client/views/core/home.js
@@ -43,26 +43,5 @@ Template.home.helpers({
       // Session.set('gameId', null);
       return false;
     }
-  },
-  playerOfTheWeek: function() {
-    var oneWeekAgo = new Date();
-    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-    oneWeekAgo = Date.parse(oneWeekAgo);
-    games = Games.find({startDate: {$gt: oneWeekAgo}}).fetch();
-    var map = {};
-    for (var i = 0; i < games.length; i++) {
-      var records = games[i].records;
-      for (var p in records){
-        if (records.hasOwnProperty(p)) {
-         if (map[p]) { map[p] = map[p] += records[p].hits; }
-         else { map[p] = records[p].hits; }
-       }
-     }
-   }
-   var max = Object.keys(map).reduce(function(max,key){
-     return (max === undefined || map[key] > map[max]) ? +key : max;
-   });
-   var potw = Meteor.users.findOne({_id: max});
-   return {name: potw.profile.name, picture: potw.profile.picture};
- }
+  }
 });
